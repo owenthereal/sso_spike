9d0429ae55c1f0bccd44cccb6c3b9e473f71dd14
Jingwen Owen Ou <jingweno@gmail.com>
2012-12-07 16:31:42 -0800

    Hook up sign in and sign up with accounts app

--- accounts/app/controllers/application_controller.rb
+++ accounts/app/controllers/application_controller.rb
@@ -1,4 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
+
+  def after_sign_in_path_for(resource_or_scope)
+    redirect_url || super(resource_or_scope)
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    redirect_url || super(resource_or_scope)
+  end
+
+  def after_sign_up_path_for(resource_or_scope)
+    redirect_url || super(resource_or_scope)
+  end
+
+  helper_method :redirect_url
+  def redirect_url
+    params[:redirect]
+  end
+
+  helper_method :redirect_url?
+  def redirect_url?
+    params.key?(:redirect)
+  end
 end
--- todos/app/controllers/application_controller.rb
+++ todos/app/controllers/application_controller.rb
@@ -1,3 +1,50 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate_user!
+
+  def authenticate_user!
+    logger.debug "TODO"
+    logger.debug session.inspect
+    logger.debug Marshal.load(Base64.decode64(cookies["_accounts_session"])).inspect
+  end
+
+  helper_method :current_user
+  def current_user
+    require 'ostruct'
+    @current_user = OpenStruct.new
+    @current_user.email = "foo@bar.com"
+
+    @current_user
+  end
+
+  helper_method :user_signed_in?
+  def user_signed_in?
+    account_session.key?("warden.user.user.key")
+  end
+
+  helper_method :current_url
+  def current_url
+    "#{request.protocol}#{request.host_with_port}#{request.fullpath}"
+  end
+
+  helper_method :sign_up_url
+  def sign_up_url
+    "#{request.protocol}accounts.#{request.domain}#{request.port_string}/users/sign_up?redirect=#{Rack::Utils.escape(current_url)}"
+  end
+
+  helper_method :sign_in_url
+  def sign_in_url
+    "#{request.protocol}accounts.#{request.domain}#{request.port_string}/users/sign_in?redirect=#{Rack::Utils.escape(current_url)}"
+  end
+
+  helper_method :sign_out_url
+  def sign_out_url
+    "#{request.protocol}accounts.#{request.domain}#{request.port_string}/users/sign_out?redirect=#{Rack::Utils.escape(current_url)}"
+  end
+
+  private
+
+  def account_session
+    @account_session ||= Marshal.load(Base64.decode64(cookies["_accounts_session"]))
+  end
 end
