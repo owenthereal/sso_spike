5fb8ab70b94c067dc9da24cc1d9ab06fd5de2d19
Jingwen Owen Ou <jingweno@gmail.com>
2012-12-12 14:31:32 -0800

    Introduce another todos app

---
+++ another_todos/app/controllers/application_controller.rb
@@ -0,0 +1,102 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery
+  before_filter :authenticate_user!
+
+  def authenticate_user!
+    logger.debug "TODO"
+    logger.debug session.inspect
+    logger.debug cookies.inspect
+    logger.debug Marshal.load(Base64.decode64(cookies["_accounts_session"])).inspect
+  end
+
+  helper_method :current_user
+  def current_user
+    require 'ostruct'
+    @current_user = OpenStruct.new
+
+    if code
+      require "net/http"
+      uri = URI.parse("#{accounts_url}/login/oauth/access_token.json")
+      resp = Net::HTTP.post_form(uri, {
+        client_id: client_id,
+        client_secret: client_secret,
+        code: code
+      })
+      logger.debug resp.inspect
+
+      access_token = JSON.parse(resp.body)["access_token"]
+      uri = URI.parse("#{accounts_url}/api/user.json?oauth_token=#{CGI.escape(access_token)}")
+      resp = Net::HTTP.get_response(uri)
+
+      logger.debug resp.inspect
+      @current_user.email = JSON.parse(resp.body)["email"]
+    else
+      @current_user.email = "unknown"
+    end
+
+    @current_user
+  end
+
+  helper_method :user_signed_in?
+  def user_signed_in?
+    accounts_cookies.key?("warden.user.user.key") && code.present?
+  end
+
+  helper_method :current_url
+  def current_url
+    "#{request.protocol}#{request.host_with_port}#{request.fullpath}"
+  end
+
+  def client_id
+    "mmhx6ugzrokogw6od2sl5k2smlbya2x"
+  end
+
+  def client_secret
+    "ioruayvoxo3ffafv6iwzsik16720c6t"
+  end
+
+  helper_method :sign_up_url
+  def sign_up_url
+    accounts_auth_url_for(:sign_up)
+  end
+
+  helper_method :sign_in_url
+  def sign_in_url
+    accounts_auth_url_for(:sign_in)
+  end
+
+  helper_method :sign_out_url
+  def sign_out_url
+    "#{request.protocol}accounts.#{request.domain}#{request.port_string}/users/sign_out?redirect_uri=#{current_url}"
+  end
+
+  private
+
+  def code
+    share_cookies
+  end
+
+  def accounts_auth_url_for(path)
+    "#{accounts_url}/users/#{path}?redirect_uri=#{current_url}&client_id=#{client_id}&response_type=code"
+  end
+
+  def accounts_url
+    "#{request.protocol}accounts.#{request.domain}#{request.port_string}"
+  end
+
+  def accounts_cookies
+    @accounts_cookies ||= Marshal.load(Base64.decode64(cookies[accounts_cookies_name])) if cookies[accounts_cookies_name]
+  end
+
+  def share_cookies
+    @share_cookies ||= cookies[share_cookies_name]
+  end
+
+  def accounts_cookies_name
+    "_accounts_session"
+  end
+
+  def share_cookies_name
+    "__#{request.host_with_port}"
+  end
+end
---
+++ another_todos/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todos::Application.config.secret_token = 'a74b2c7a48e18a38e4cd61ab8dea889fc27a09ae9617e5da95450dc980e0e10538192b72653526df7af0a0c1f9bebbbc43a6929e1750784bbbc47b3bcfc17ae4'
